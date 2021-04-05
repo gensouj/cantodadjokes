fc71f2b5da0e29c0095562b34e6a4146769ac86d
James Deng <jamesdeng89@gmail.com>
2021-04-04 23:41:08 -0400

    added new styling for the jokes and added spoiler logic and hardcoded jokes

--- src/app/jokes/jokes.component.ts
+++ src/app/jokes/jokes.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JokesComponent implements OnInit {
 
+  jokes = [
+    {
+      question: 'Why did the guy who lost all his vegetables resort to a life of crime?',
+      answer: 'He was left with no choys.',
+      jyutpings: [
+        {
+          ping: 'coi',
+          tone: 3
+        }
+      ],
+      character: '菜s',
+      revealed: false
+    },
+    {
+      question: 'Why did the cop start shooting after the suspect farted?',
+      answer: 'Because he had to hoy cherng.',
+      jyutpings: [
+        {
+          ping: 'hoi',
+          tone: 1
+        },
+        {
+          ping: 'coeng',
+          tone: 1
+        }
+      ],
+      character: '開槍/開窗',
+      revealed: false
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.jokes);
   }
-
 }
